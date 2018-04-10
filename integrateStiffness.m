0139534bc53c6e3bf3de20c4e9e938587c5ffea9
HarshGehani <33762936+HarshGehani@users.noreply.github.com>
2018-04-09 23:19:54 -0400

    Integrates elemental stiffness matrix equations 2D

---
+++ integrateStiffness.m
@@ -0,0 +1,26 @@
+function [stiffnessMatrix] = integrateStiffness(equationMatrix)
+    % function integrates eta and zeta of the elemental stiffness matrix
+    % and returns its numerical value
+    
+    [m,n] = size(equationMatrix);
+    
+    % integration limits:
+    etaMin = -1;
+    etaMax = 1;
+    zetaMin = -1;
+    zetaMax = 1;
+    
+    % iterate over each cell of elemental stiffness matrix:
+    for i=1:m
+        for j=1:n
+            fun = matlabFunction(equationMatrix(i,j));
+            
+            % check function for zero integral:
+            if ~isequal(func2str(fun),func2str(@()0.0))
+                stiffnessMatrix(i,j) = integral2(fun,etaMin,etaMax,zetaMin,zetaMax);
+            else
+                stiffnessMatrix(i,j) = 0;
+        end
+    end
+    
+end
